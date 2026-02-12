681f13a55dc176f804e136d3ef09baffa9100b9e
Winston Wu <winxton@gmail.com>
2026-02-11 20:08:39 -0500

    Set Vite server port to 6100 in vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   plugins: [tailwindcss(), sveltekit()],
   server: {
+    port: 6100,
     proxy: {
       "/api": "http://localhost:3000",
     },
