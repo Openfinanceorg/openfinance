8ef7c09eebcadd55aa87e994b127cce7865da892
Winston Wu <winxton@gmail.com>
2026-02-15 21:17:14 -0500

    Setup tests with DB.

--- server/vitest.config.ts
+++ server/vitest.config.ts
@@ -5,6 +5,18 @@ export default defineConfig({
   resolve: {
     alias: {
       $lib: path.resolve(__dirname, "src/lib"),
+      "@shared": path.resolve(__dirname, "src/shared"),
+    },
+  },
+  test: {
+    env: {
+      DATABASE_URL:
+        "postgresql://postgres:postgres@localhost:5433/openfin_test",
+    },
+    globalSetup: "./src/tests/test-setup.ts",
+    pool: "forks",
+    poolOptions: {
+      forks: { singleFork: true },
     },
   },
 });
